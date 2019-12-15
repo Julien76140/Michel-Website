2ee14c421f89babb22e30ab411ee9287f6f96124
Julien <sam@google.com>
2019-12-15 16:29:18 +0100

    Michel ajoute les autres catégories du menu nav

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,8 @@
     <ul>
         <li><a href="index.php?page=accueil">Accueil</a></li>
         <li><a href="index.php?page=cv">Curriculum Vitae</a></li>
+        <li><a href="index.php?page=contact">Contact</a></li>
+        <li><a href="index.php?page=mention">Mention légale</a></li>
 
     </ul>
 </nav>
